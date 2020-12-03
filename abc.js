940bf812874d57bbb89680e78d668c2071f4175f
nam <ceonamnguyen@gmail.com>
2020-12-03 17:02:50 +0700

    first

--- abc.js
+++ abc.js
@@ -1,1 +1,25 @@
-consolsd
+consovar nameInput = document.getElementById('name')
+var dobInput = document.getElementById('dob')
+var tableList = document.getElementById("list")
+
+var tdName = document.createElement("td")
+tdName.innerText = nameInput.value;
+
+var tdDOB = document.createElement("td")
+tdDOB.innerText = dobInput.value;
+
+var tr = document.createElement("tr")
+
+var trList = tableList.getElementsByTagName("tr")
+var lastTr = trList[trList.length - 1]
+
+if (lastTr.className === "even") {
+    tr.className = "odd"
+} else {
+    tr.className = "even"
+}
+
+tr.appendChild(tdName)
+tr.appendChild(tdDOB)
+tableList.appendChild(tr)
+lsd
